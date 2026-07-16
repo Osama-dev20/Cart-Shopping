96e74e64ebacd34ff3da2c678af0ce48cd85cd83
Osama Alkatib <yunisashraf@gmail.com>
2026-07-16 16:16:10 +0300

    feat: implement add to cart functionality

--- src/js/main.js
+++ src/js/main.js
@@ -11,5 +11,39 @@ Xclose.addEventListener("click", () => {
 })
 
 
+cartContent = document.querySelector(".cart-content")
+addCartBtn = document.querySelectorAll(".add-cart")   // More than one element
+addCartBtn.forEach(button => {
+  button.addEventListener("click", event => {
+     const productBox = event.target.closest(".Product-box")
+     addtoCart(productBox)
+  });
+});
+
+const addtoCart = productBox => {
+  const productImg = productBox.querySelector("img").src;
+  const productTitle = productBox.querySelector(".Product-title").textContent;
+  const productPrice = productBox.querySelector(".price").textContent
+
+  const cartBox = document.createElement("div")
+  cartBox.classList.add("cart-box")
+  cartBox.innerHTML = `
+                 <img src="${productImg}" >
+              <div class="cart-detail">
+                   <h5 class="cart-product-title">${productTitle}</h5>
+                   <span class="cart-product-price">${productPrice}</span>
+                   <div class="cart-quantity">
+                     <button id="decrement">-</button>
+                     <span class="number">0</span>
+                     <button id="increment">+</button>
+                   </div>
+              </div>
+               <i class="ri-delete-bin-line cart-remove"></i>
+  
+  `;
+  cartContent.appendChild(cartBox);
+}
+
+
 
 
